c4daf82b3527872ff8d7873ae33b3e3d40ac9559
Alok Kumar <36418056+alokkiitd1729@users.noreply.github.com>
2021-10-02 03:16:03 +0530

    Update FWHT.cpp

--- Math/FWHT.cpp
+++ Math/FWHT.cpp
@@ -74,105 +74,50 @@ int32_t main() {
   return 0;
 }
 // https://csacademy.com/contest/archive/task/and-closure/statement/
-
-
-
+########################################################################  short clean for -        AND/OR/XOR
 
 #include <bits/stdc++.h>
-
-
 using namespace std;
-
-#define mp make_pair
-#define pb push_back
 #define ll long long
-
 #define sz (1 << 20)
 #define maxN (sz + 11)
 #define mod 1000000007
-
-ll n, i, x;
-vector<ll> data1(maxN,0);
-
-int cnt;
-
-void moddo(ll &x) {
-    while (x >= mod)
-        x -= mod;
-}
-
-void to_transform(ll dim) {
-    ll len, i, j;
-    ll u, v;
-
-    for (len = 1; 2 * len <= dim; len <<= 1) {
-        for (i = 0; i < dim; i += 2 * len) {
-            for (j = 0; j < len; j++) {
-                u = data1[i + j];
-                v = data1[i + len + j];
-
-                data1[i + j] = v;
-                data1[i + len + j] = (u + v);
-
-                moddo(data1[i + len + j]);
-            }
-        }
-    }
-}
-
-void inv_transform(ll dim) {
-    ll len, i, j;
-    ll u, v;
-
-    for (len = 1; 2 * len <= dim; len <<= 1) {
-        for (i = 0; i < dim; i += 2 * len) {
-            for (j = 0; j < len; j++) {
-                u = data1[i + j];
-                v = data1[i + len + j];
-
-                data1[i + j] = mod - u + v;
-                data1[i + len + j] = u;
-
-                moddo(data1[i + j]);
-            }
-        }
-    }
-}
-
+int AND=0,OR=1,XOR=2;
+ll n, x,cnt=0;
+vector<ll> a(maxN,0);
 ll poww(ll a, ll b) {
-    ll ans = 1;
-
-    while (b > 0) {
-        if (b & 1) ans = (ans * a) % mod;
-        b >>= 1;
-        a = (a * a) % mod;
-    }
-
-    return ans;
+    if(b<=0) return 1;
+    ll c=poww(a,b/2); c*=c;c%=mod;
+    return b%2?c*a%mod:c;
 }
-
-
-
-int main()
-{
-    scanf("%lld", &n);
-    for (i = 1; i <= n; i++) {
-        scanf("%lld", &x);
-        data1[x]++;
+void FST(bool inv,int flag){
+    for(int step=1;2*step<=sz;step*=2){
+        for(int i=0;i<sz;i+=2*step){
+            for(int j=0;j<step;j++){
+                ll &u=a[j+i],&v=a[j+step+i];
+                if(flag==AND)tie(u,v)=inv?make_pair((v-u+mod)%mod,u):make_pair(v,(u+v)%mod);//AND
+                if(flag==OR)tie(u,v)=inv?make_pair(v,(u-v+mod)%mod):make_pair((u+v)%mod,v);//OR
+                if(flag==XOR)tie(u,v)=make_pair((u+v)%mod,(u-v+mod)%mod);
+            }
+        }
     }
-
-    data1[0]++;
-
-    to_transform(sz);
-    for (i = 0; i < sz; i++) data1[i] = poww(data1[i], n);
-    inv_transform(sz);
-
-    for (i = 0; i < sz; i++)
-        if (data1[i] != 0)
-            cnt++;
-
-    printf("%d\n", cnt);
-
+    if(inv&&flag==XOR){
+        for(int i=0;i<sz;i++){
+            a[i]/=sz;  // may be mod inv?
+        }
+    }
+}
+int main() {
+    cin>>n;
+    for (int i = 1; i <= n; i++) {
+        cin>>x; a[x]++;
+    }
+    a[0]++;
+    FST(0,AND);
+    for (int i = 0; i < sz; i++) a[i] = poww(a[i], n);
+    FST(1,AND);
+    for (int i = 0; i < sz; i++)
+        if (a[i] != 0) cnt++;
+    cout<<cnt;
     return 0;
 }
-
